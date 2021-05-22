7ed809db25afabf5c169f64ec84dbff899f95f6b
pashokmi <pashokmi@gmail.com>
2021-05-22 16:21:37 +0300

    added design

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,12 @@
 $(function () {
-  
+
   $('.slider__inner').slick({
     dots: true,
     arrows: false,
-    fade:true
+    fade: true
   })
 
   var mixer = mixitup('.showcase__content');
+
+  var mixer = mixitup('.design__content');
 })
