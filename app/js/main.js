643155481fd87c177ba977360aa3c199efc98669
pashokmi <pashokmi@gmail.com>
2021-06-10 14:11:53 +0300

    added media home page

--- app/js/main.js
+++ app/js/main.js
@@ -38,3 +38,50 @@ $(function () {
 $('.menu__btn').on('click', function () {
   $('.menu__list').toggleClass('menu__list--active');
 });
+
+$(function () {
+  $('.partners__inner').slick({
+    dots: false,
+    arrows: false,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay:true,
+    autoplaySpeed: 1000,
+    swipeToSlide: true,
+     responsive: [{
+         breakpoint: 1024,
+         settings: {
+           slidesToShow: 4,
+           slidesToScroll: 1,
+           infinite: true,
+           
+         }
+       },
+       {
+         breakpoint: 768,
+         settings: {
+           slidesToShow: 3,
+           slidesToScroll: 2
+         }
+       },
+       {
+         breakpoint: 564,
+         settings: {
+           slidesToShow: 2,
+           slidesToScroll: 1
+         }
+       }, 
+       {
+         breakpoint: 480,
+         settings: {
+           slidesToShow:1 ,
+           slidesToScroll: 1
+         }
+       }
+       // You can unslick at a given breakpoint now by adding:
+       // settings: "unslick"
+       // instead of a settings object
+     ]
+  });
+
+})
